043b7914b60fcc80130ecc9014bf2f1728655df3
mandeepbhutani <bhutanimandeep@gmail.com>
2016-09-29 21:42:45 -0500

    Changed variable names

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -6,30 +6,30 @@ def binary_search(sorted_list, value):
     value -- a float, integer, or string to be found in the sorted list
 
     Analysis:
-    The low variable is set to the sorted list's first index, while the
-    high variable is set to the sorted list's last index. The middle variable
-    is the middle index of the sorted list. We can determine which part of the
+    The first variable is set to the sorted list's first index, while the
+    last variable is set to the sorted list's last index. The middle variable
+    is the sorted list's middle index. We can determine which part of the
     sorted list the value argument resides by comparing it to the element at
     the middle of the sorted list. If the value argument is less than the
-    middle element, the high variable is assigned the middle value's index
+    middle element, the last variable is assigned the middle value's index
     minus 1 since the middle element does not equal the value argument. If the
     value argument is greater than the element at the middle of the sorted
-    list, the low variable is assigned the index of the middle element plus 1
-    since the middle element does not equal the value argument. The low or high
-    variable continues to change until the value argument equals the middle
-    element. If the value argument is found, its index is returned. Otherwise,
-    the function will return None.
+    list, the first variable is assigned the index of the middle element plus 1
+    since the middle element does not equal the value argument. The first or
+    last variable continues to change until the value argument equals the
+    middle element. If the value argument is found, its index is returned.
+    Otherwise, the function will return None.
 
-    For further reading: https://en.wikipedia.org/wiki/Binary_search_algorithm
+    Reference: https://en.wikipedia.org/wiki/Binary_search_algorithm
     """
-    low = 0
-    high = len(sorted_list) - 1
-    while low <= high:
-        middle = (high + low) // 2
+    first = 0
+    last = len(sorted_list) - 1
+    while first <= last:
+        middle = (last + first) // 2
         if value < sorted_list[middle]:
-            high = middle - 1
+            last = middle - 1
         elif value > sorted_list[middle]:
-            low = middle + 1
+            first = middle + 1
         else:
             return middle
     return None
