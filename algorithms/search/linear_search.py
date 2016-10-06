eb01d29260dcac11caf87a292758a63ab1432333
mandeepbhutani <bhutanimandeep@gmail.com>
2016-10-05 23:15:53 -0500

    Changed first line of docstring

--- algorithms/search/linear_search.py
+++ algorithms/search/linear_search.py
@@ -1,12 +1,12 @@
-def linear_search(unordered_list, value):
-    """Given an unordered list and value, returns the index of the value.
+def linear_search(unsorted_list, value):
+    """Search an unsorted list for the given value and return its index.
 
     Positional arguments:
     unodered list -- a list of values of any type
     value -- the element to be found in the list
 
     Analysis:
-    Starting at the beginning of the unordered list, the value of each
+    Starting at the beginning of the unsorted list, the value of each
     element is checked against the value of the value argument. Once
     there is a match, the index of the element is returned. If the
     value argument is not contained within the list, None is returned.
@@ -29,7 +29,7 @@ def linear_search(unordered_list, value):
 
     Reference: https://en.wikipedia.org/wiki/Linear_search
     """
-    for index, element in enumerate(unordered_list):
+    for index, element in enumerate(unsorted_list):
         if element == value:
             return index
     return None
