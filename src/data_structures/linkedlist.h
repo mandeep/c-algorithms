99cbbf81eff208a9ab8154e28febc8b511ca3f9a
mandeep <mandeep@users.noreply.github.com>
2017-10-23 22:05:52 -0500

    Add functions to delete nodes, destroy the linked list, and print the linked list

--- src/data_structures/linkedlist.h
+++ src/data_structures/linkedlist.h
@@ -21,14 +21,6 @@ void initialize_list(linked_list *list) {
 }
 
 
-void free_node(node *n) {
-    if (n != NULL) {
-        free(n);
-        n = NULL;
-    }
-}
-
-
 void insert_at_tail(linked_list *list, size_t number) {
     node *np = malloc(sizeof(node));
     np->value = number;
@@ -70,4 +62,76 @@ void insert_in_order(linked_list *list, size_t number) {
 
     previous->next = np;
     np->next = current;
+}
+
+
+void free_node(node *n) {
+    if (n != NULL) {
+        free(n);
+        n = NULL;
+    }
+}
+
+
+void delete_at_head(linked_list *list) {
+    node *old = list->head;
+    list->head = list->head->next;
+    free_node(old);
+}
+
+
+void delete_at_tail(linked_list *list) {
+    node *previous = NULL;
+    node *current = list->head;
+
+    if (list->head->next == NULL) {
+        node *old = list->head;
+        list->head = NULL;
+        free_node(old);
+    } else {
+        while (current->next != NULL) {
+            previous = current;
+            current = current->next;
+        }
+        previous->next = NULL;
+        free_node(current);
+    }
+}
+
+
+void delete_node(linked_list *list, size_t element) {
+    node *previous = NULL;
+    node *current = list->head;
+
+    if (list->head->value == element) {
+        node *old = list->head;
+        list->head = list->head->next;
+        free_node(old);
+    } else {
+        while (current != NULL && current->value != element) {
+            previous = current;
+            current = current->next;
+        }
+        previous->next = current->next;
+        free_node(current);
+    }
+}
+
+
+void destroy_list(linked_list *list) {
+    while (list->head != NULL) {
+        node *destroyed_node = list->head;
+        list->head = list->head->next;
+        free_node(destroyed_node);
+    }
+}
+
+
+void print_list(linked_list *list) {
+    node *current = list->head;
+    while (current != NULL) {
+        printf("%zu ", current->value);
+        current = current->next;
+    }
+    printf("\n");
 }
