7889974f9244d58a42522303ee6bcf23761dfdfd
mandeep <mandeep@users.noreply.github.com>
2017-10-25 21:48:35 -0500

    Fix pop function st->top equality check

--- src/abstract_data_types/stack_array.h
+++ src/abstract_data_types/stack_array.h
@@ -25,7 +25,9 @@ void push(stack *st, int number) {
 
 int pop(stack *st) {
     int top_element = st->array[st->top];
-    st->top -= 1;
+    if (st->top > -1) {
+        st->top -= 1;    
+    }
 
     return top_element;
 }
