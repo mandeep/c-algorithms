e9bb7dcdd98cd5f83fc6e8afcf884173a0f010c2
mandeep <mandeep@users.noreply.github.com>
2017-10-28 00:51:01 -0500

    Add doc comments to structs and functions

--- src/data_structures/linkedlist.h
+++ src/data_structures/linkedlist.h
@@ -3,19 +3,37 @@
 #include <stdlib.h>
 
 
+/**
+* node - node data type to be used with the linked list
+*
+* @value: the element to hold in the node
+* @next: the next node to link to when creating a singly linked list
+* @previous: the previous node to link if creating a doubly linked list
+*/
 typedef struct node {
     size_t value;
-    struct node *previous;
     struct node *next;
+    struct node *previous;
 } node;
 
 
+/**
+* linked_list - the linked_list data structure that holds nodes of data
+*
+* @head: the head node of the linked list
+* @tail: the tail node of the linked list (used when keeping track of the last node) 
+*/
 typedef struct linked_list {
     node *head;
     node *tail;
 } linked_list;
 
 
+/**
+* initialize_list - allocate memory for a new linked list
+*
+* Returns: linked_list
+*/
 linked_list *initialize_list(void) {
     linked_list *list = malloc(sizeof(node) * 2);
 
@@ -26,9 +44,22 @@ linked_list *initialize_list(void) {
 }
 
 
-void insert_at_tail(linked_list *list, size_t number) {
+/**
+* insert_at_tail - insert a node at the tail end of the linked list
+*
+* @list: the linked_list in which to insert the node
+* @value: the value of the element to insert into the linked list
+*
+* Returns: void
+*
+* This method of inserting nodes use both the head and tail nodes
+* of the linked_list in order to keep track of the head node
+* and tail node separately. This method is not to be used with
+* other insertion methods.
+*/
+void insert_at_tail(linked_list *list, size_t value) {
     node *np = malloc(sizeof(node));
-    np->value = number;
+    np->value = value;
     np->next = NULL;
 
     if (list->head == NULL) {
@@ -41,17 +72,41 @@ void insert_at_tail(linked_list *list, size_t number) {
 }
 
 
-void insert_at_head(linked_list *list, size_t number) {
+/**
+* insert_at_head - insert a node at the head of the linked list
+*
+* @list: the linked_list in which to insert the node
+* @value: the value of the element to insert into the linked list
+*
+* Returns: void
+*
+* This method of inserting nodes uses only the head node of
+* the linked_list. Thus, nodes are inserted at the head of the
+* linked list. This method is not to be used with other insertion
+* methods.
+*/
+void insert_at_head(linked_list *list, size_t value) {
     node *np = malloc(sizeof(node));
-    np->value = number;
+    np->value = value;
     np->next = list->head;
     list->head = np;
 }
 
 
-void insert_at_position(linked_list *list, size_t index, size_t number) {
+/**
+* insert_at_position - insert node at the given position of the linked list
+*
+* @list: the linked_list in which to insert the node
+* @value: the value of the element to insert into the linked list
+* @index: the index at which to insert the node
+*
+* Returns: void
+*
+* This method is not to be used with other insertion methods.
+*/
+void insert_at_position(linked_list *list, size_t value, size_t index) {
     node *np = malloc(sizeof(node));
-    np->value = number;
+    np->value = value;
     node *previous = NULL;
     node *head = list->head;
 
@@ -79,14 +134,24 @@ void insert_at_position(linked_list *list, size_t index, size_t number) {
 }
 
 
-void insert_in_order(linked_list *list, size_t number) {
+/**
+* insert_in_order - insert a node into the linked list in sorted order
+*
+* @list: the linked_list in which to insert the node
+* @value: the value of the element to insert into the linked list
+*
+* Returns: void
+*
+* This method is not to be used with other insertion methods.
+*/
+void insert_in_order(linked_list *list, size_t value) {
     node *np = malloc(sizeof(node));
-    np->value = number;
+    np->value = value;
     np->next = NULL;
     node *previous = NULL;
     node *current = list->head;
 
-    while (current != NULL && number > current->value) {
+    while (current != NULL && value > current->value) {
         previous = current;
         current = current->next;
     }
@@ -101,6 +166,14 @@ void insert_in_order(linked_list *list, size_t number) {
 }
 
 
+/**
+* get_node - get the node at the given index
+*
+* @list: the linked_list in which to retrieve the node
+* @index: the index of the node to retrieve
+*
+* Returns: the node at the given index
+*/
 node *get_node(linked_list *list, size_t index) {
     node *current = list->head;
     size_t count = 0;
@@ -118,6 +191,15 @@ node *get_node(linked_list *list, size_t index) {
 }
 
 
+/**
+* swap_nodes - swap two nodes in the given linked list
+*
+* @list: the linked_list in which to swap nodes
+* @x: the value of the node to swap with y
+* @y: the value of the node to swap with x
+*
+* Returns: void
+*/
 void swap_nodes(linked_list *list, size_t x, size_t y) {
     node *current_y = list->head;
     node *current_x = list->head;
@@ -158,12 +240,20 @@ void swap_nodes(linked_list *list, size_t x, size_t y) {
 }
 
 
-size_t count_node_occurences(linked_list *list, size_t number) {
+/**
+* count_node_occurrences - count the number of times the given value appears in the linked list
+*
+* @list: the linked_list in which to count node occurrences
+* @value: the value of the element of which to count occurrences
+*
+* Returns: void
+*/
+size_t count_node_occurences(linked_list *list, size_t value) {
     node *current = list->head;
     size_t count = 0;
 
     while (current != NULL) {
-        if (current->value == number) {
+        if (current->value == value) {
             count += 1;
         }
         current = current->next;
@@ -173,6 +263,13 @@ size_t count_node_occurences(linked_list *list, size_t number) {
 }
 
 
+/**
+* free_node - free the memory that was allocated for the given node
+*
+* @node: the node whose memory to free
+*
+* Returns: void
+*/
 void free_node(node **n) {
     if (*n != NULL && n != NULL) {
         free(*n);
@@ -181,6 +278,13 @@ void free_node(node **n) {
 }
 
 
+/**
+* delete_at_head - remove the node at the head of the linked list
+*
+* @list: the linked_list whose head node will be removed
+*
+* Returns: void
+*/
 void delete_at_head(linked_list *list) {
     node *destroyed_node = list->head;
     list->head = list->head->next;
@@ -188,6 +292,13 @@ void delete_at_head(linked_list *list) {
 }
 
 
+/**
+* delete_at_tail - remove the node at the tail end of the linked list
+*
+* @list: the linked_list whose last node will be removed
+*
+* Returns: void
+*/
 void delete_at_tail(linked_list *list) {
     node *previous = NULL;
     node *current = list->head;
@@ -207,16 +318,24 @@ void delete_at_tail(linked_list *list) {
 }
 
 
-void delete_node(linked_list *list, size_t element) {
+/**
+* delete_node - remove the given element of the linked list
+*
+* @list: the linked_list whose node will be removed
+* @value: the value of the node to remove from the linked list
+*
+* Returns: void
+*/
+void delete_node(linked_list *list, size_t value) {
     node *previous = NULL;
     node *current = list->head;
 
-    if (list->head->value == element) {
+    if (list->head->value == value) {
         node *destroyed_node = list->head;
         list->head = list->head->next;
         free_node(&destroyed_node);
     } else {
-        while (current != NULL && current->value != element) {
+        while (current != NULL && current->value != value) {
             previous = current;
             current = current->next;
         }
@@ -226,6 +345,13 @@ void delete_node(linked_list *list, size_t element) {
 }
 
 
+/**
+* remove_sorted_duplicates - remove duplicate nodes from a sorted linked list
+*
+* @list: the linked_list whose duplicate nodes will be removed
+*
+* Returns: void
+*/
 void remove_sorted_duplicates(linked_list *list) {
     node *current = list->head;
     node *subsequent = list->head->next;
@@ -245,6 +371,13 @@ void remove_sorted_duplicates(linked_list *list) {
 }
 
 
+/**
+* destroy_list - free the memory allocated for the linked list
+*
+* @list: the linked_list whose memory will be freed
+*
+* Returns: void
+*/
 void destroy_list(linked_list **list) {
     while ((*list)->head != NULL) {
         node *destroyed_node = (*list)->head;
@@ -259,6 +392,13 @@ void destroy_list(linked_list **list) {
 }
 
 
+/**
+* reverse_list - reverse the nodes of the linked list
+*
+* @list: the linked_list whose nodes to reverse
+*
+* Returns: void
+*/
 void reverse_list(linked_list *list) {
     node *previous = NULL;
     node *current = list->head;
@@ -274,6 +414,14 @@ void reverse_list(linked_list *list) {
 }
 
 
+/**
+* merged_sorted_lists - merged two sorted lists into a new sorted linked list
+*
+* @list1: the first sorted linked list to merge
+* @list2: the second sorted linked list to merge
+*
+* Returns: a sorted linked list containing the values of list1 and list2
+*/
 linked_list *merge_sorted_lists(linked_list *list1, linked_list *list2) {
     linked_list *merged_list = initialize_list();
 
@@ -311,6 +459,13 @@ linked_list *merge_sorted_lists(linked_list *list1, linked_list *list2) {
 }
 
 
+/**
+* is_list_sorted - determine whether or not a linked list is sorted
+*
+* @list: the linked_list to check for orderness
+*
+* Returns: boolean value of linked list orderness
+*/
 bool is_list_sorted(linked_list *list) {
     node *current = list->head;
 
@@ -326,6 +481,13 @@ bool is_list_sorted(linked_list *list) {
 }
 
 
+/**
+* list_length - the number of nodes in the linked list
+*
+* @list: the linked_list whose nodes to count
+*
+* Returns: the number of nodes in the linked list
+*/
 size_t list_length(linked_list *list)  {
     size_t length = 0;
     node *current = list->head;
@@ -339,6 +501,13 @@ size_t list_length(linked_list *list)  {
 }
 
 
+/**
+* print_list - print the contents of the linked list
+*
+* @list: the linked_list whose values to print
+*
+* Returns: void
+*/
 void print_list(linked_list *list) {
     node *current = list->head;
 
