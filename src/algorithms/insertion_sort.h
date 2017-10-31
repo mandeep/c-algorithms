9fe4c2089fe3989051c3a1f53c7103c0b4d04cbd
mandeep <mandeep@users.noreply.github.com>
2017-10-31 10:48:57 -0500

    Add doc comments to insertion sort algorithm

--- src/algorithms/insertion_sort.h
+++ src/algorithms/insertion_sort.h
@@ -2,12 +2,29 @@
 #include <stdlib.h>
 
 
+/**
+* swap - swap the values of the given parameters
+*
+* @a: the variable whose value will be changed to b's value
+* @b: the variable whose value will be changed to a's value
+*
+* Returns: void
+*/
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
+
+/**
+* insertion_sort - traverse an array while bringing the smallest values to the front
+*
+* @array: the array to sort
+* @length: the length of the array
+*
+* Returns: void
+*/
 void insertion_sort(int array[], int length) {
     for (int i = 1; i < length; ++i) {
         while (i > 0 && array[i] < array[i-1]) {
