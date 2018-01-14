224b87a5c93f5cbb3ad0d551d564b596028e591a
mandeep <mandeep@users.noreply.github.com>
2018-01-13 21:34:22 -0500

    Add iterative gcd algorithm to euclidean_algorithm.c

--- src/algorithms/euclidean_algorithm.c
+++ src/algorithms/euclidean_algorithm.c
@@ -1,15 +1,33 @@
 /**
-* gcd - find the greatest common divisor of the two input parameters
+* recursive_gcd - find the greatest common divisor of the two input parameters
 *
 * @a: one of the numbers to be used in the gcd calculation
 * @b: the other number to be used in the gcd calculation
 *
 * Returns: the greatest common divisor of the two input parameters
 */
-int gcd(int a, int b) {
+int recursive_gcd(int a, int b) {
     if (b == 0) {
         return a;
     } else {
-        return gcd(b, a % b);
+        return recursive_gcd(b, a % b);
     }
+}
+
+
+/**
+* iterative_gcd - find the greatest common divisor of the two input parameters
+*
+* @a: one of the numbers to be used in the gcd calculation
+* @b: the other number to be used in the gcd calculation
+*
+* Returns: the greatest common divisor of the two input parameters
+*/
+int iterative_gcd(int a, int b) {
+    while (b > 0) {
+        int temp = a;
+        a = b;
+        b = temp % b;
+    }
+    return a;
 }
