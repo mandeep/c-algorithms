2ec2a5f3e26241a7f3c472dae4d5921b0bcdd640
mandeep <mandeep@users.noreply.github.com>
2018-02-28 15:44:11 -0600

    Add doc comments to functions

--- src/data_structures/binary_search_tree.c
+++ src/data_structures/binary_search_tree.c
@@ -1,6 +1,14 @@
 #include "binary_search_tree.h"
 
 
+
+/**
+* create_node: create a new node that will be inserted into the binary search tree
+*
+* @value: the value, or key, that the node will hold
+*
+* Returns: the newly created node
+*/
 node *create_node(size_t value) {
     node *np = malloc(sizeof(node));
     np->value = value;
@@ -10,6 +18,14 @@ node *create_node(size_t value) {
 }
 
 
+/**
+* insert_node: create a new node with the passed value into the passed root node
+*
+* @root: the root node that will contain the new node
+* @value: the value, or key, to assign to the inserted node
+*
+* Returns: void
+*/
 void insert_node(node **root, size_t value) {
     if (*root == NULL) {
         *root = create_node(value);
@@ -23,6 +39,14 @@ void insert_node(node **root, size_t value) {
 }
 
 
+/**
+* find_node: find the node with the passed value within the passed root node
+*
+* @root: the root node of the binary tree to search
+* @value: the value, or key, to search for
+*
+* Returns: the node with the passed value or NULL
+*/
 node *find_node(node *root, size_t value) {
     if (root == NULL || root->value == value) {
         return root;
@@ -34,6 +58,13 @@ node *find_node(node *root, size_t value) {
 }
 
 
+/**
+* minimum: find the node with the minimum value within the passed root node
+*
+* @root: the root node of the binary tree to search
+*
+* Returns: the node that contains the minimum value
+*/
 node *minimum(node *root) {
     if (root->left == NULL) {
         return root;
@@ -43,6 +74,13 @@ node *minimum(node *root) {
 }
 
 
+/**
+* maximum: find the node with the maximum value within the passed root node
+*
+* @root: the root node of the binary tree to search
+*
+* Returns: the node that contains the maximum value
+*/
 node *maximum(node *root) {
     if (root->right == NULL) {
         return root;
@@ -52,6 +90,13 @@ node *maximum(node *root) {
 }
 
 
+/**
+* preorder_traversal: traverse the passed root node in preorder
+*
+* @root: the root node of the binary tree to traverse
+*
+* Returns: void
+*/
 void preorder_traversal(node *root) {
     if (root != NULL) {
         printf("%zu\n", root->value);
@@ -61,6 +106,13 @@ void preorder_traversal(node *root) {
 }
 
 
+/**
+* inorder_traversal: traverse the passed root node in order
+*
+* @root: the root node of the binary tree to traverse
+*
+* Returns: void
+*/
 void inorder_traversal(node *root) {
     if (root != NULL) {
         inorder_traversal(root->left);
@@ -70,6 +122,13 @@ void inorder_traversal(node *root) {
 }
 
 
+/**
+* preorder_traversal: traverse the passed root node in postorder
+*
+* @root: the root node of the binary tree to traverse
+*
+* Returns: void
+*/
 void postorder_traversal(node *root) {
     if (root != NULL) {
         postorder_traversal(root->left);
