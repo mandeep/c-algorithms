41f9a34fcaf8f4b58d271632b4f686fe84303aa8
mandeep <mandeep@users.noreply.github.com>
2018-03-01 00:03:06 -0600

    Add height, count_nodes, and count_leaves functions

--- src/data_structures/binary_search_tree.c
+++ src/data_structures/binary_search_tree.c
@@ -66,7 +66,6 @@ node *delete_node(node *root, size_t value) {
     if (root == NULL) {
         return root;
     }
-
     if (value < root->value) {
         root->left = delete_node(root->left, value);
     } else if (value > root->value) {
@@ -136,6 +135,67 @@ node *maximum(node *root) {
     } else {
         return maximum(root->right);
     }
+}
+
+
+/**
+* height: find the height of the binary search tree
+*
+* @root: the root node of the binary search tree
+*
+* Returns: the height of the binary search tree
+*/
+int height(node *root) {
+    if (root == NULL) {
+        return -1;
+    }
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    if (left > right) {
+        return left + 1;
+    } else {
+        return right + 1;
+    }
+}
+
+
+/**
+* count_nodes: count the number of nodes in the binary search tree
+*
+* @root: the binary search tree whose nodes to count
+*
+* Returns: the number of nodes in the tree
+*/
+size_t count_nodes(node *root) {
+    if (root == NULL) {
+        return 0;
+    } else {
+        return count_nodes(root->left) + count_nodes(root->right) + 1;
+    }
+}
+
+
+/**
+* count_leaves: count the number of leaves in the binary search tree
+*
+* @root: the binary search tree whose leaves to count
+*
+* Returns: the number of leaves in the tree
+*/
+size_t count_leaves(node *root) {
+    size_t count = 0;
+    if (root->left == NULL && root->right == NULL) {
+        count += 1;
+    }
+    if (root->left != NULL) {
+        count += count_leaves(root->left);
+    }
+    if (root->right !=NULL) {
+        count += count_leaves(root->right);
+    }
+    return count;
 }
 
 
