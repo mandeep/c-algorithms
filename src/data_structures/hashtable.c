62e13341099be150091428b37a88f3c7a8129f6b
mandeep <mandeep@users.noreply.github.com>
2018-04-08 23:06:33 -0500

    Add hash table to data structures

---
+++ src/data_structures/hashtable.c
@@ -0,0 +1,106 @@
+#include "hashtable.h"
+
+
+/**
+* initialize_hashtable - allocate memory for the hashtable and initialize the members
+*
+* @capacity: the maximum number of elements allowed to be stored in the hashtable
+*
+* Returns: the initialized hashtable
+*/
+hashtable *initialize_hashtable(size_t capacity) {
+    hashtable *table = malloc(sizeof(hashtable) + capacity * sizeof(member));
+    table->size = capacity;
+    table->count = 0;
+
+    for (size_t i = 0; i < capacity; i++) {
+        table->members[i].key = 0;
+        table->members[i].value = NULL;
+    }
+
+    return table;
+}
+
+
+/**
+* insert_key - insert a key and corresponding value into the hashtable
+*
+* @table: the hashtable in which to insert the key and value
+* @key: the key to store inside the hashtable
+* @value: the value to store alongside the given key
+*
+* Returns: void
+*/
+void insert_key(hashtable *table, size_t key, char *value) {
+    size_t i = 0;
+    size_t hash = ((key % table->size) + i * i) % table->size;
+
+    if (table->count < table->size) {
+        if (table->members[hash].value != NULL && table->members[hash].key != key) {
+            while (table->members[hash].value != NULL) {
+                i += 1;
+                hash = ((key % table->size) + i * i) % table->size;
+            }
+        }
+        table->members[hash].key = key;
+        table->members[hash].value = value;
+        table->count += 1;
+    }
+}
+
+
+/**
+* remove_key - remove a key and corresponding value from the hashtable
+*
+* @table: the hashtable in which to remove the given key
+* @key: the key to remove from the hashtable
+*
+* Returns: void
+*/
+void remove_key(hashtable *table, size_t key) {
+    size_t i = 0;
+    size_t hash = ((key % table->size) + i * i) % table->size;
+
+    if (table->members[hash].key != key) {
+        size_t count = 0;
+        while (count < table->size && table->members[hash].key != key) {
+            i += 1;
+            hash = ((key % table->size) + i * i) % table->size;
+            count += 1;
+        }
+    }
+
+    table->members[hash].key = 0;
+    table->members[hash].value = NULL;
+}
+
+
+/**
+* print_table - print to stdout the contents of the hashtable
+*
+* @table: the hashtable whose contents to print to stdout
+*
+* Returns: void
+*/
+void print_table(hashtable *table) {
+    for (size_t i = 0; i < table->size - 1; i++) {
+        if (table->members[i].key != 0 && table->members[i].value != NULL) {
+            printf("Key: %zu, Value: %s\n", table->members[i].key, table->members[i].value);
+        }
+    }
+}
+
+
+/**
+* destroy_table - free the memory allocated for the hashtable
+*
+* @table: the hashtable to whose memory to free
+*
+* Returns: void
+*/
+void destroy_table(hashtable **table) {
+    if (table != NULL && *table != NULL) {
+        free(*table);
+        *table = NULL;
+    }
+} 
---
+++ src/data_structures/hashtable.h
@@ -0,0 +1,44 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+* member - the key and value pair to store inside the hashtable array
+*
+* @key: an unsigned integer to store as the key to the hashtable element
+* @value: a string to store inside the hashtable element
+*/
+typedef struct member {
+    size_t key;
+    char *value;
+} member;
+
+
+/**
+* hashtable - the hashtable in which to store keys and values as members
+*
+* @count: the number of members stored within the hashtable
+* @size: the maximum number of members allowed in the hashtable
+* @members: the array of members that contain the key and value
+*/
+typedef struct hashtable {
+    size_t count;
+    size_t size;
+    member members[];
+} hashtable;
+
+hashtable *initialize_hashtable(size_t capacity);
+
+void insert_key(hashtable *table, size_t key, char *value);
+
+void remove_key(hashtable *table, size_t key);
+
+void print_table(hashtable *table);
+
+void destroy_table(hashtable **table);
+
+
+#endif
