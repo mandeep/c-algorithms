ff46a8c3dfa50e4b00f61fa24558634e34f562c4
mandeep <mandeep@users.noreply.github.com>
2018-04-24 00:27:32 -0500

    Move main function to test_suite.c and change array1_length variable to use sizeof

--- tests/test_sorts.c
+++ tests/test_sorts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "greatest.h"
 
 #include "../src/algorithms/bubble_sort.c"
@@ -8,12 +9,11 @@
 #include "../src/algorithms/heap_sort.c"
 
 
-int tests_run = 0;
-
 int array1[] = {-40897, 84, 0, 3029, 92, 1904, 3498, -3924, -32, 1,
                 39809324, -1089, 5023, 232409, -23, -249, 789, 378546,
                 8934, 789524, 8475, 47589275, 89475897};
-size_t array1_length = 23;
+
+size_t array1_length = sizeof(array1) / sizeof(int);
 
 int *intcpy(int array[], size_t length) {
     int *copy = malloc(length * sizeof(int));
@@ -66,12 +66,4 @@ SUITE(sorting_algorithms) {
     RUN_TEST(test_bubble_sort);
     RUN_TEST(test_counting_sort);
     RUN_TEST(test_heap_sort);
-}
-
-GREATEST_MAIN_DEFS();
-
-int main(int argc, char **argv) {
-    GREATEST_MAIN_BEGIN();
-    RUN_SUITE(sorting_algorithms);
-    GREATEST_MAIN_END();
 }
