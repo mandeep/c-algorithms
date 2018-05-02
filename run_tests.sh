bccd4f7b29b472152cbe341c97c2e28b39a669e8
mandeep <mandeep@users.noreply.github.com>
2018-05-01 20:03:46 -0500

    Add removal of test runner binary after test suite completes

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc tests/test_suite.c -Wall -Wextra -pedantic -o test_suite -std=c11
-./test_suite -v | tests/greenest.sh
+gcc tests/test_suite.c -Wall -Wextra -pedantic -o test_suite -std=c11 &&
+./test_suite -v | tests/greenest.sh &&
+rm test_suite
