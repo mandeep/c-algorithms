8735ebaa56811f468d2538b814a51ef23f989fe1
mandeep <mandeep@users.noreply.github.com>
2018-05-05 02:42:29 -0500

    Add statistical algorithms suite

--- tests/test_suite.c
+++ tests/test_suite.c
@@ -1,5 +1,6 @@
 #include "test_searches.c"
 #include "test_sorts.c"
+#include "test_statistics.c"
 
 
 GREATEST_MAIN_DEFS();
@@ -8,5 +9,6 @@ int main(int argc, char **argv) {
     GREATEST_MAIN_BEGIN();
     RUN_SUITE(sorting_algorithms);
     RUN_SUITE(searching_algorithms);
+    RUN_SUITE(statistical_algorithms);
     GREATEST_MAIN_END();
 }
