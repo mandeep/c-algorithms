c238287e04666282df22d80f4157e59854197133
mandeep <mandeep@users.noreply.github.com>
2018-05-05 02:52:22 -0500

    Run statistical algorithms test suite first

--- tests/test_suite.c
+++ tests/test_suite.c
@@ -7,8 +7,8 @@ GREATEST_MAIN_DEFS();
 
 int main(int argc, char **argv) {
     GREATEST_MAIN_BEGIN();
+    RUN_SUITE(statistical_algorithms);
     RUN_SUITE(sorting_algorithms);
     RUN_SUITE(searching_algorithms);
-    RUN_SUITE(statistical_algorithms);
     GREATEST_MAIN_END();
 }
