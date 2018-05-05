47203e2b7ffbbe761e22a8865b7aa10eaf7e6a1a
mandeep <mandeep@users.noreply.github.com>
2018-05-05 02:33:21 -0500

    Add header guard for swap and partition functions

--- src/algorithms/statistics.c
+++ src/algorithms/statistics.c
@@ -1,3 +1,7 @@
+#ifndef SWAP_FUNC_
+#define SWAP_FUNC_
+
+
 /**
 * swap - swap the values of the given parameters
 *
@@ -11,7 +15,14 @@ void swap(int* a, int* b) {
     *a = *b;
     *b = temp;
 }
+
+
+#endif
  
+
+#ifndef PARTITION_FUNC_
+#define PARTITION_FUNC_
+
 
 /**
 * partition - partition the subarray around the pivot element
@@ -35,6 +46,9 @@ int partition(int array[], int low, int high) {
     swap(&array[i + 1], &array[high]);
     return i + 1;
 }
+
+
+#endif
 
 
 /**
