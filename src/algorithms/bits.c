9b60318314cf4e68c05a1e8034d7c27d06203023
mandeep <mandeep@users.noreply.github.com>
2018-06-17 19:20:53 -0700

    Add algorithms dealing with binary bits

---
+++ src/algorithms/bits.c
@@ -0,0 +1,36 @@
+/**
+* count_ones - count the number of one bits in the given integer
+*
+* @n: the integer whose bits to count
+*
+* Returns: an integer of the number of one bits in the argument
+*/
+int count_ones(int n) {
+    int ones = 0;
+
+    while (n > 0) {
+        ones += 1;
+        n &= n - 1;
+    }
+
+    return ones;
+}
+
+
+/**
+* check_parity - check the parity of the given integer's bits
+*
+* @n - the integer whose parity to check
+*
+* Returns: 1 if the number of one bits is odd, 0 if the number of one bits is even
+*/
+int check_parity(int n) {
+    int parity = 0;
+
+    while (n > 0) {
+        parity ^= 1;
+        n &= n - 1;
+    }
+
+    return parity;
+}
