d0b5adda0df58c7e000bbb031f9298a19311817d
mandeep <mandeep@users.noreply.github.com>
2018-08-18 18:31:47 -0500

    Add swap bits function

--- src/algorithms/bits.c
+++ src/algorithms/bits.c
@@ -34,3 +34,23 @@ int check_parity(int n) {
 
     return parity;
 }
+
+
+/**
+ * swap_bits - swap the bits at index i and index j
+ *
+ * @n: the interger whose bits to swap
+ * @i: the index of the bit to swap with the bit at index j
+ * @j: the index of the bit to swap with the bit at index i
+ *
+ * Since bits are shifted from the end, the i and j indexes
+ * are counted from the right not from the left.
+ */
+int swap_bits(int n, int i, int j) {
+    if ((n >> i & 1) != (n >> j & 1)) {
+        int bit_mask = (1 << i) | (1 << j);
+        n ^= bit_mask;
+    }
+
+    return n;
+}
