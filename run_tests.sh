c60abc0b2a6faeae59d80056dd86f3887fd9b6d2
mandeep <mandeep@users.noreply.github.com>
2018-08-20 01:26:13 -0500

    Add gcov and coveralls support

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-gcc tests/test_suite.c -Wall -Wextra -pedantic -o test_suite -std=c11 -lm &&
+gcc tests/test_suite.c -Wall -Wextra -pedantic -std=c11 -lm -o test_suite --coverage &&
 ./test_suite -v | tests/greenest.sh &&
 rm test_suite
+gcov tests/test_suite.c -o .
