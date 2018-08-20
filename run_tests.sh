be8977d7378383078c9f3db8fab00b2acfa83e5e
mandeep <mandeep@users.noreply.github.com>
2018-08-20 15:18:06 -0500

    Add command line option for coverage

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 gcc tests/test_suite.c -Wall -Wextra -pedantic -std=c11 -lm -o test_suite --coverage &&
-./test_suite -v | tests/greenest.sh &&
-gcov tests/test_suite.c -o .
-if [ $# -eq 1 ] && [ "$1" == "--remove" ]; then
+./test_suite -v | tests/greenest.sh
+
+if [ $# -gt 0 ] && [ "$1" == "--coverage" ]; then
+    gcov tests/test_suite.c -o .
+fi
+
+if [ $# -gt 1 ] && [ "$2" == "--remove" ]; then
     rm test_suite
-    rm *.gc*
+    rm ./*.gc*
 fi
