9e5a8351e6ba290a18a96f12d3ceef7c5fcbbf4e
mandeep <mandeep@users.noreply.github.com>
2018-08-20 15:14:20 -0500

    Complete test stubs for bits.c

--- tests/test_bits.c
+++ tests/test_bits.c
@@ -3,5 +3,19 @@
 #include "../src/algorithms/bits.c"
 
 
+TEST test_bit_algorithms(void) {
+    ASSERT_EQ(count_ones(8), 1);
+    ASSERT_EQ(count_ones(63), 6);
+    ASSERT_EQ(check_parity(10), 0);
+    ASSERT_EQ(check_parity(11), 1);
+    ASSERT_EQ(swap_bits(10, 2, 3), 6);
+    ASSERT_EQ(swap_bits(6, 0, 1), 5);
+    ASSERT_EQ(reverse_bits(10), 5);
+    ASSERT_EQ(reverse_bits(11), 13);
+    PASS();
+}
+
+
 SUITE(bit_algorithms) {
+    RUN_TEST(test_bit_algorithms);
 }
