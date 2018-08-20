0724b3b7a03e6e44d753c633e646afa55ffec97a
mandeep <mandeep@users.noreply.github.com>
2018-08-20 10:59:02 -0500

    Add test stub for hash table

---
+++ tests/test_ht.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "greatest.h"
+
+#include "../src/data_structures/hashtable.c"
+
+
+SUITE(hash_table) {
+}
--- tests/test_suite.c
+++ tests/test_suite.c
@@ -3,6 +3,7 @@
 #include "test_statistics.c"
 #include "test_euclidean.c"
 #include "test_bst.c"
+#include "test_ht.c"
 #include "test_reverse_digits.c"
 #include "test_remove_duplicates.c"
 #include "test_palindrome.c"
@@ -18,6 +19,7 @@ int main(int argc, char **argv) {
     RUN_SUITE(searching_algorithms);
     RUN_SUITE(euclidean_algorithms);
     RUN_SUITE(binary_search_tree);
+    RUN_SUITE(hash_table);
     RUN_SUITE(reverse_algorithms);
     RUN_SUITE(removal_algorithms);
     RUN_SUITE(palindrome_algorithms);
