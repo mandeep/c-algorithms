270df8989db1ab41c636e4521ff29d8b98807b52
mandeep <mandeep@users.noreply.github.com>
2018-08-20 15:27:16 -0500

    Add more tests for ith_order_statistic

--- tests/test_statistics.c
+++ tests/test_statistics.c
@@ -9,8 +9,12 @@
 
 TEST test_ith_order_statistic(void) {
     int stat_array[] = {1, 2, 3, 4, 5};
-    int index = ith_order_statistic(stat_array, 0, 5, 1);
-    ASSERT_EQ_FMT(index, 0, "%d");
+    int i = ith_order_statistic(stat_array, 0, 5, 1);
+    int j = ith_order_statistic(stat_array, 0, 0, 0);
+    int k = ith_order_statistic(stat_array, 0, 5, 4);
+    ASSERT_EQ_FMT(i, 0, "%d");
+    ASSERT_EQ_FMT(j, 0, "%d");
+    ASSERT_EQ_FMT(k, 3, "%d");
     PASS();
 }
 
