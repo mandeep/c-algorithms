d622f25d35b7f5996dfc87349df529cbe2fac795
mandeep <mandeep@users.noreply.github.com>
2018-08-20 15:33:55 -0500

    Add test for find_first_occurrence

--- tests/test_searches.c
+++ tests/test_searches.c
@@ -7,22 +7,33 @@
 #include "../src/algorithms/searching.c"
 
 
-int search_array[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 24, 26, 28, 30};
+int search_array[] = {2, 4, 6, 8, 10, 12, 12, 12, 14, 16, 18, 20, 24, 26, 28, 30};
 int array_length = sizeof(search_array) / sizeof(int);
+
 
 TEST test_binary_search(void) {
     int index = binary_search(search_array, array_length, 12);
-    ASSERT_EQ(index, 5);
+    ASSERT_EQ_FMT(index, 7, "%d");
     PASS();
 }
+
+
+TEST test_find_first_occurrence(void) {
+    int index = find_first_occurrence(search_array, array_length, 12);
+    ASSERT_EQ_FMT(index, 5, "%d");
+    PASS();
+}
+
 
 TEST test_linear_search(void) {
     int index = linear_search(search_array, array_length, 30);
-    ASSERT_EQ(index, 13);
+    ASSERT_EQ_FMT(index, 15, "%d");
     PASS();
 }
 
+
 SUITE(searching_algorithms) {
     RUN_TEST(test_binary_search);
+    RUN_TEST(test_find_first_occurrence);
     RUN_TEST(test_linear_search);
 }
