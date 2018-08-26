8082419a03598d46d5750717a70358698c678d73
mandeep <mandeep@users.noreply.github.com>
2018-08-26 14:22:32 -0500

    Add tests for array based queue

--- tests/test_queue_array.c
+++ tests/test_queue_array.c
@@ -3,5 +3,29 @@
 #include "../src/abstract_data_types/queue_array.c"
 
 
+TEST test_queue_array(void) {
+    queue_array *queue = initialize_queue_array(3);
+
+    qa_enqueue(queue, 3);
+    qa_enqueue(queue, 4);
+    qa_enqueue(queue, 5);
+
+    qa_print(queue);
+
+    ASSERT_EQ(qa_is_empty(queue), 0);
+
+    qa_dequeue(queue);
+    qa_dequeue(queue);
+    qa_dequeue(queue);
+
+    ASSERT_EQ(qa_is_empty(queue), 1);
+
+    qa_destroy(&queue);
+    PASS();
+}
+
+
+
 SUITE(array_based_queue) {
+    RUN_TEST(test_queue_array);
 }
