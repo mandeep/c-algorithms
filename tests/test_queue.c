8e9a73a8f91232280b13eb7e7ed380adaa385ee3
mandeep <mandeep@users.noreply.github.com>
2018-08-26 20:20:55 -0500

    Add test for linked list based queue

--- tests/test_queue.c
+++ tests/test_queue.c
@@ -3,5 +3,28 @@
 #include "../src/abstract_data_types/queue_linkedlist.c"
 
 
+TEST test_queue(void) {
+    queue *q = initialize_queue();
+    ASSERT_EQ(q_is_empty(q), 1);
+
+    q_enqueue(q, (size_t*) 1);
+    q_enqueue(q, (size_t*) 2);
+    q_enqueue(q, (size_t*) 3);
+    q_enqueue(q, (size_t*) 4);
+    q_enqueue(q, (size_t*) 5);
+
+    ASSERT_EQ(q->list->head->value, (size_t*) 1);
+
+    q_dequeue(q);
+
+    ASSERT_EQ(q->list->head->value, (size_t*) 2);
+
+    q_print(q);
+    q_destroy(&q);
+    PASS();
+}
+
+
 SUITE(queue_linkedlist) {
+    RUN_TEST(test_queue);
 }
