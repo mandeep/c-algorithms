ca5b08ef1b1b03d0b00e7fa86e3815793228fcd7
mandeep <mandeep@users.noreply.github.com>
2018-08-26 20:40:45 -0500

    Add test for double ended queue

--- tests/test_dequeue.c
+++ tests/test_dequeue.c
@@ -3,5 +3,35 @@
 #include "../src/abstract_data_types/dequeue.c"
 
 
+TEST test_deque(void) {
+    dequeue *deq = initialize_dequeue();
+
+    deq_push(deq, (size_t*) 1);
+    deq_push(deq, (size_t*) 2);
+    deq_push(deq, (size_t*) 3);
+    deq_push(deq, (size_t*) 4);
+    deq_push(deq, (size_t*) 5);
+    deq_pushleft(deq, (size_t*) 0);
+
+    ASSERT_EQ(deq->list->tail->value, (size_t*) 5);
+    ASSERT_EQ(deq->list->head->value, (size_t*) 0);
+
+    deq_pop(deq);
+    print_dequeue(deq);
+
+    deq_popleft(deq);
+
+    deq_remove_value(deq, (size_t*) 3);
+
+    ASSERT_EQ(deq_length(deq), 3);
+    ASSERT_EQ(deq_count(deq, (size_t*) 2), 1);
+
+    destroy_dequeue(&deq);
+    PASS();
+}
+
+
+
 SUITE(doubleended_queue) {
+    RUN_TEST(test_deque);
 }
