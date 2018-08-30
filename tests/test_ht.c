a86a7af532449b2b1db1f4e0187989c76a68528c
mandeep <mandeep@users.noreply.github.com>
2018-08-30 16:26:29 -0400

    Use new hash table implementation

--- tests/test_ht.c
+++ tests/test_ht.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include <stdlib.h>
 
 #include "greatest.h"
@@ -9,30 +10,22 @@
 TEST test_hashtable(void) {
     hashtable *table = ht_new(5);
 
-    char *values[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-    for (int i = 0; i < 5; i++) {
-        ht_insert(table, i, values[i]);
+    char *values[] = {"abc", "cba", "dkhga", "aahfa", "dskjha", "ashf", "gda", "ah", "ha", "haa"};
+
+    for (size_t i = 0; i < 5; i++) {
+        ht_insert(table, values[i], (intptr_t*) i);
     }
 
-    ht_remove(table, 2);
+    ht_remove(table, "dkhga");
     ht_resize(&table, 10);
 
-    for (int i = 5; i < 10; i++) {
-        ht_insert(table, i, values[i]);
+    for (size_t j = 5; j < 10; j++) {
+        ht_insert(table, values[j], (intptr_t*) j);
     }
 
-    ASSERT_EQ_FMT(ht_search(table, 0), "a", "%s");
-    ASSERT_EQ_FMT(ht_search(table, 1), "b", "%s");
-    ASSERT_EQ_FMT(ht_search(table, 3), "d", "%s");
-    ASSERT_EQ_FMT(ht_search(table, 4), "e", "%s");
-    ASSERT_EQ_FMT(ht_search(table, 5), "f", "%s");
-    ASSERT_EQ_FMT(ht_search(table, 6), "g", "%s");
-    ASSERT_EQ_FMT(ht_search(table, 7), "h", "%s");
-    ASSERT_EQ_FMT(ht_search(table, 8), "i", "%s");
-    ASSERT_EQ_FMT(ht_search(table, 9), "j", "%s");
-    ASSERT_EQ(ht_search(table, 2), NULL);
+    ASSERT_EQ((intptr_t) ht_search(table, "abc"), 0);
+    ASSERT_EQ((intptr_t) ht_search(table, "haa"), 9);
 
-    ht_print(table);
     ht_destroy(&table);
     PASS();
 }
