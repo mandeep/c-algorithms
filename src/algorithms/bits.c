29b2e39f1422153d3698534ccb50eb8ffe4d2698
mandeep <mandeep@users.noreply.github.com>
2018-09-18 01:13:12 -0400

    Add int to binary function

--- src/algorithms/bits.c
+++ src/algorithms/bits.c
@@ -76,3 +76,19 @@ int reverse_bits(int n) {
 
     return m;
 }
+
+
+/**
+ * int_to_binary - given an integer n, place the bits of n in to the given array
+ *
+ * @n: the integer whose bits to find
+ * @array: the array in which to place the bits
+ *
+ * Returns: void
+ */
+void int_to_binary(int n, int array[]) {
+    for (int i = 31; i >= 0; i--) {
+        array[i] = n & 1;
+        n >>= 1;
+    }
+}
